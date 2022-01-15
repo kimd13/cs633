8488648586a65a45756895eade3b91716012af36
Daniel Kim <developer.danielkim@gmail.com>
2022-01-15 08:01:07 -0800

    initial commit

---
+++ app/src/main/java/edu/bu/cs633/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.bu.cs633
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
