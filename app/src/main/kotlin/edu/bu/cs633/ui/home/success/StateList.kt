0f88128424cc92ac0b406904b8eaa68843eb3087
Daniel Kim <developer.danielkim@gmail.com>
2022-02-13 16:57:01 -0800

    added card style formatting to data

--- app/src/main/kotlin/edu/bu/cs633/ui/home/success/StateList.kt
+++ app/src/main/kotlin/edu/bu/cs633/ui/home/success/StateList.kt
@@ -1,20 +1,25 @@
 package edu.bu.cs633.ui.home.success
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import edu.cs633.data.domain.VaccinationRecord
 
 @Composable
 fun StateList(
     vaccinationRecords: List<VaccinationRecord>
 ) {
-    LazyColumn {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+    ) {
         items(vaccinationRecords) { record ->
-            Text(
-                text = record.location ?: "Not Available"
-            )
+            StateCard(vaccinationRecord = record)
         }
     }
 }
